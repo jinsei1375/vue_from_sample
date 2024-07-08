95553d0a918c1eae369afe1856b7335b3fa2fb11
murakami <murakami@kokochie.jp>
2024-07-08 11:10:32 +0900

    ヘッダーフッターコンポーネント作成

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,22 @@
 <template>
-  <router-view></router-view>
+  <div id="app">
+    <Header></Header>
+    <router-view></router-view>
+    <Footer></Footer>
+  </div>
 </template>
+
+<script>
+  import Header from './components/Header.vue';
+  import Footer from './components/Footer.vue';
+
+  export default {
+    name: 'App',
+    components: {
+      Header,
+      Footer
+    }
+  }
+</script>
 
 
--- src/router/index.js
+++ src/router/index.js
@@ -3,8 +3,10 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Form from '../Form.vue'
 import Confirm from '../Confirm.vue'
 import Completed from '../Completed.vue'
+import Home from '../Home.vue'
 
 const routes = [
+  { path: '/', name: 'Home', component: Home },
   { path: '/form', name: 'Form', component: Form },
   { path: '/form/confirm', name: 'Confirm', component: Confirm },
   { path: '/form/completed', component: Completed }
