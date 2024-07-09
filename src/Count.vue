67553699f9880060e1eff2fe2e8fec42641bd92b
murakami <murakami@kokochie.jp>
2024-07-09 10:04:13 +0900

    カウントサンプル

---
+++ src/Count.vue
@@ -0,0 +1,23 @@
+<script setup>
+    import { ref } from 'vue';
+    const count = ref(0);
+
+    // カウントアップ
+    const increment = () => {
+        count.value++;
+    };
+
+    // カウントダウン
+    const decrement = () => {
+        count.value--;
+    };
+</script>
+
+<template>
+    <div>
+        <h1>Count</h1>
+        <p>{{ count }}</p>
+        <button @click="increment">カウントアップ</button>
+        <button @click="decrement">カウントダウン</button>
+    </div>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -4,12 +4,14 @@ import Form from '../Form.vue'
 import Confirm from '../Confirm.vue'
 import Completed from '../Completed.vue'
 import Home from '../Home.vue'
+import Count from '../Count.vue'
 
 const routes = [
   { path: '/', name: 'Home', component: Home },
   { path: '/form', name: 'Form', component: Form },
   { path: '/form/confirm', name: 'Confirm', component: Confirm },
-  { path: '/form/completed', component: Completed }
+  { path: '/form/completed', name: 'Count', component: Completed },
+  { path: '/count', name: 'Count', component: Count },
 ]
 
 const router = createRouter({
